47fdb27fe15112112792dd2e673688d8ea0b3867
Ben Goumalatsos <bengoumalatsos@hotmail.com>
2020-12-25 22:17:20 -0500

    Added ability to receive text input

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("hi\n");
+  FILE *fp;
+  fp = fopen("/Users/ben/Desktop/myfile.txt", "w");
+
+  char password[100];
+  printf("\nEnter password: ");
+  scanf("%s", password);  
+
+  printf("\nYou entered: %s\n", password);
+
   return 0;
 }
