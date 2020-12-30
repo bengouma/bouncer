ad6eaa358cbe6a1e3fa3c59ff1f8fa257c929b20
bengouma <bengoumalatsos@hotmail.com>
2020-12-29 21:18:32 -0500

    Add flag options

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 char * encryptPass(char *pass) {
-  for (int i = 0; i < 4; i++) {
+  for (int i = 0; i < 11; i++) {
     pass[i]++;
   } 
   return pass;
@@ -9,12 +9,43 @@ char * encryptPass(char *pass) {
 
 int main(void) {
   FILE *fp;
-  fp = fopen("/Users/ben/Desktop/myfile.txt", "w");
+  fp = fopen("/home/ben/Desktop/myfile.txt", "w");
 
-  char password[4];
-  printf("\nEnter password: ");
-  scanf("%s", password);
-  printf("Your encrypted pass: %s\n", encryptPass(password));
+  char key[30];
+  char site[30];
+  char password[30];
+  char option[1];
+
+  printf("\nChoose option (l,n,d,c): ");
+  scanf("%s", option);
+
+  if(*option == 'l') {
+    printf("\nEnter key: ");
+    scanf("%s", key);
+
+    if(key == "abc") {
+      printf("hi\n");
+    }
+
+  } else if (*option == 'n') {
+
+    printf("\nEnter site: ");
+    scanf("%s", site);
+    fprintf(fp, "%s\n", site);
+
+    printf("\nEnter password: ");
+    scanf("%s", password);
+    fprintf(fp, "%s\n", encryptPass(password));
+
+  } else if (*option == 'd') {
+    printf("delete\n");
+  } else if (*option == 'c') {
+    printf("change\n");
+  } else {
+    printf("select valid option\n");
+  }
+
+  fclose(fp);
 
   return 0;
 }
